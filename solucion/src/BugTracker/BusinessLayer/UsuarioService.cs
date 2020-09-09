933355577c6a43aad1669f04c907708f50cd94b7
Ruben A. Romero <romeroa.ruben@gmail.com>
2020-09-09 18:23:36 -0300

    Solucion

---
+++ solucion/src/BugTracker/BusinessLayer/UsuarioService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugTracker.Entities;
+using BugTracker.DataAccessLayer;
+
+namespace BugTracker.BusinessLayer
+{
+    public class UsuarioService
+    {
+        private UsuarioDao oUsuarioDao;
+        public UsuarioService()
+        {
+            oUsuarioDao = new UsuarioDao();
+        }
+        public IList<Usuario> ObtenerTodos()
+        {
+            return oUsuarioDao.GetAll();
+        }
+
+        public Usuario ValidarUsuario(string usuario, string password)
+        {
+            var usr = oUsuarioDao.GetUser(usuario);
+
+            if (usr.Password != null && usr.Password.Equals(password))
+            {
+                return usr;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ solucion/src/BugTracker/DataAccessLayer/UsuarioDao.cs
@@ -0,0 +1,72 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using BugTracker.Entities;
+using System.Data;
+
+namespace BugTracker.DataAccessLayer
+{
+    public class UsuarioDao
+    {
+        public IList<Usuario> GetAll()
+        {
+            List<Usuario> listadoBugs = new List<Usuario>();
+
+            var strSql = "SELECT id_usuario, usuario, email, estado from Usuarios";
+
+            var resultadoConsulta = DataManager.GetInstance().ConsultaSQL(strSql);
+
+            foreach (DataRow row in resultadoConsulta.Rows)
+            {
+                listadoBugs.Add(MappingBug(row));
+            }
+
+            return listadoBugs;
+        }
+
+        public Usuario GetUser(string pUsuario)
+        {        
+            //Construimos la consulta sql para buscar el usuario en la base de datos.
+            String consultaSql = string.Concat(" SELECT id_usuario, usuario, email, estado, password ",
+                                               "   FROM Usuarios ",
+                                               "  WHERE usuario =  '", pUsuario, "'");
+
+            //Usando el método GetDBHelper obtenemos la instancia unica de DBHelper (Patrón Singleton) y ejecutamos el método ConsultaSQL()
+            var resultado = DataManager.GetInstance().ConsultaSQL(consultaSql);
+
+            // Validamos que el resultado tenga al menos una fila.
+             if (resultado.Rows.Count > 0)
+            {
+                return MappingBug(resultado.Rows[0]);
+            }
+
+            return null;
+        }
+
+        private Usuario MappingBug(DataRow row)
+        {
+            Usuario oUsuario = new Usuario
+            {
+                IdUsuario = Convert.ToInt32(row["id_usuario"].ToString()),
+                NombreUsuario = row["usuario"].ToString(),
+                Email = row["email"].ToString(),
+                Estado = row["estado"].ToString(),
+                Password = row.Table.Columns.Contains("password") ? row["password"].ToString() : null
+            };
+
+            return oUsuario;
+        }
+    }
+
+}
